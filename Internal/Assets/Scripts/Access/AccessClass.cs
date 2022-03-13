4b859bf4cb31141791194f8cb042c5667238c221
eungyukm <86348187+eungyukm@users.noreply.github.com>
2022-03-13 23:19:56 +0900

    프로젝트 추가

---
+++ Internal/Assets/Scripts/Access/AccessClass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var myBase = new BaseClass();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
